ce026734f6ac4616e2fbce998692fdf24f0b8a27
Chang Hyun Park <heartinpiece@gmail.com>
2018-08-26 21:58:40 +0900

    Updates list.h file

--- list.h
+++ list.h
@@ -6,9 +6,11 @@ struct list_node {
 };
 
 // Basic list functions
-int insert_node (struct list_node *node, struct list_node *new_node);
-int del_node (struct list_node *node);
-struct list_node *search_list (int search_key);
+struct list_node *allocate_node_with_key(int key);
+void initialize_list_head_tail(struct list_node *head, struct list_node *tail);
+void insert_node_after (struct list_node *node, struct list_node *new_node);
+void del_node (struct list_node *node);
+struct list_node *search_list (struct list_node *head, int search_key);
 
 int count_list_length (struct list_node *head);
 int is_list_empty (struct list_node *head);
