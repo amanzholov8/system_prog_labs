324f114139cc27f6b2fa7f0f41d862bc881295f3
Alim Amanzholov <amanzholov@kaist.ac.kr>
2018-09-05 17:49:32 +0900

    My implementation of Lab1

--- list.c
+++ list.c
@@ -11,14 +11,25 @@ set the key value to the key provided by the argument
  */
 struct list_node *allocate_node_with_key(int key)
 {
+  struct list_node* node = (struct list_node*)malloc(sizeof(struct list_node));
+  node->prev = NULL;
+  node->next = NULL;
+  node->key = key;
+  return node;
 }
 
-/*	
+/*
 Initialize the key values of the head/tail list nodes (I used -1 key values)
 Also, link the head and tail to point to each other
  */
 void initialize_list_head_tail(struct list_node *head, struct list_node *tail)
 {
+  head->key = -1;
+  tail->key = -1;
+  head->next = tail;
+  tail->prev = head;
+  head->prev = tail;
+  tail->next = head;
 }
 
 /*	
@@ -26,6 +37,10 @@ Insert the *new_node* after the *node*
  */
 void insert_node_after (struct list_node *node, struct list_node *new_node)
 {
+  node->next->prev = new_node;
+  new_node->next = node->next;
+  node->next = new_node;
+  new_node->prev = node;
 }
 
 /*	
@@ -33,6 +48,8 @@ Remove the *node* from the list
  */
 void del_node (struct list_node *node)
 {
+  node->prev->next = node->next;
+  node->next->prev = node->prev;
 }
 
 /*	
@@ -44,6 +61,11 @@ You may assume that the list will only hold nodes with unique key values
  */
 struct list_node *search_list (struct list_node *head, int search_key)
 {
+  struct list_node *node = head; 
+  while (node->next != head) {
+    if (node->key == search_key) return node;
+    node = node->next;
+  }
 }
 
 /*	
@@ -52,6 +74,13 @@ and return the counted value
  */
 int count_list_length (struct list_node *head)
 {
+  int count = 0;
+  struct list_node *node = head;
+  while (node->next != head->prev) {
+    count ++;
+    node = node->next;
+  }
+  return count;
 }
 
 /*	
@@ -60,6 +89,8 @@ Return 1 if empty. Return 0 if list is not empty.
  */
 int is_list_empty (struct list_node *head)
 {
+  if (head->next == head->prev) return 1;
+  return 0; 
 }
 
 /*	
@@ -79,4 +110,11 @@ list (including the key of the *new_node*) is always sorted (increasing order)
  */
 int insert_sorted_by_key (struct list_node *head, struct list_node *new_node)
 {
+  struct list_node *node = head; 
+  while (node->next != head->prev  && node->next->key < new_node->key ) {
+    node = node->next;
+  }
+  insert_node_after(node, new_node);
+  return 1;
 }
+
