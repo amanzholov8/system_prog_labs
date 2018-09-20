db0d4a80c5fe5357efd656a5217f8d904f0c49fc
Alim Amanzholov <amanzholov@kaist.ac.kr>
2018-09-20 22:22:56 +0900

    My Implementation of Data Lab

--- bits.c
+++ bits.c
@@ -1,7 +1,8 @@
+
 /* 
  * CS:APP Data Lab 
  * 
- * <Please put your name and userid here>
+ * <Please put your name and userid here> Alim Amanzholov (cs20160845)
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
@@ -171,7 +172,8 @@ NOTES:
  *   Rating: 1
  */
 int bitAnd(int x, int y) {
-  return 2;
+   //(x and y) == not ((not x) or (not y)) 
+   return ~((~x) | (~y));
 }
 /* 
  * getByte - Extract byte n from word x
@@ -182,7 +184,11 @@ int bitAnd(int x, int y) {
  *   Rating: 2
  */
 int getByte(int x, int n) {
-  return 2;
+  //n bytes to bits (multiply by 8=2^3 i.e. shift left by 3)
+  int nbits;
+  nbits = (n<<3);
+  //shift x by nbits to the right, then & with 11111111 (to get only one byte)
+  return (x>>nbits) & 0xff;
 }
 /* 
  * logicalShift - shift x to the right by n, using a logical shift
@@ -193,7 +199,9 @@ int getByte(int x, int n) {
  *   Rating: 3 
  */
 int logicalShift(int x, int n) {
-  return 2;
+  //to change arithmetic to logical shift we need to make first n shifted bits 0 
+  int nzeros = ~(((1<<31)>>n)<<1);
+  return (x>>n) & nzeros;
 }
 /*
  * bitCount - returns count of number of 1's in word
@@ -203,7 +211,19 @@ int logicalShift(int x, int n) {
  *   Rating: 4
  */
 int bitCount(int x) {
-  return 2;
+  //divide 32 bits into 8 pieces of 4 bits by using hide
+  int hide = (0x11) | (0x11<<8) | (0x11<<16) | (0x11<<24);
+  int count = 0;
+  int count_total;
+  //first, count number of 1s in least significant bit positions of each 4 bit piece
+  count = count + (x & hide);
+  //then, count number of 1s in 2 position of each 4 bit piece and so on
+  count = count + ((x>>1) & hide);
+  count = count + ((x>>2) & hide);
+  count = count + ((x>>3) & hide);
+  //finally, add up number of 1s of each 4 bit piece
+  count_total = (count & 0xf) + ((count>>4) & 0xf) + ((count>>8) & 0xf) + ((count>>12) & 0xf) + ((count>>16) & 0xf) + ((count>>20) & 0xf) + ((count>>24) & 0xf) + ((count>>28) & 0xf);
+  return count_total;
 }
 /* 
  * bang - Compute !x without using !
@@ -213,7 +233,11 @@ int bitCount(int x) {
  *   Rating: 4 
  */
 int bang(int x) {
-  return 2;
+  // neg_x = -x, 
+  int neg_x = (~x) + 1;
+  //if x != 0, then (sign bit of x) | (sign bit of neg_x) will be 1 and its ~ will be 0; 
+  //if x == 0, ~((sign bit of x) | (sign bit of neg_x)) = 1
+  return ((~(x | neg_x)) >> 31) & 1;
 }
 /* 
  * tmin - return minimum two's complement integer 
@@ -222,7 +246,8 @@ int bang(int x) {
  *   Rating: 1
  */
 int tmin(void) {
-  return 2;
+  // tmin is 100...0 with 31 zeros, so we need to shift left 0001 by 31 bits
+  return 1<<31;
 }
 /* 
  * fitsBits - return 1 if x can be represented as an 
@@ -234,7 +259,13 @@ int tmin(void) {
  *   Rating: 2
  */
 int fitsBits(int x, int n) {
-  return 2;
+  //shift = 32 - n, the number of bits to the right of n least bits
+  int shift = 32 + (~n) + 1; 
+  //extend last n bits into 32 bits
+  int new = (x << shift) >> shift;
+  //diff = new - x, check if diff is 0, if not 0, then we cannot fit x into n bits
+  int diff = new + (~x) + 1;
+  return !diff; 
 }
 /* 
  * divpwr2 - Compute x/(2^n), for 0 <= n <= 30
@@ -245,7 +276,11 @@ int fitsBits(int x, int n) {
  *   Rating: 2
  */
 int divpwr2(int x, int n) {
-    return 2;
+  //bias = 2^n - 1 = (1<<n) - 1 = (1<<n) + (~0)
+  //we only need bias when x is negative
+  int bias;
+  bias = ((1<<n) + (~0)) & (x>>31);
+  return (x + bias)>>n;
 }
 /* 
  * negate - return -x 
@@ -255,7 +290,8 @@ int divpwr2(int x, int n) {
  *   Rating: 2
  */
 int negate(int x) {
-  return 2;
+  //-x = ~x + 1
+  return (~x) + 1;
 }
 /* 
  * isPositive - return 1 if x > 0, return 0 otherwise 
@@ -265,7 +301,8 @@ int negate(int x) {
  *   Rating: 3
  */
 int isPositive(int x) {
-  return 2;
+  //if x=0, !x=1 and result is 0; if x<0, !(x>>31)=0 and result is 0; if x>0, x>>31=0 and result is 1 
+  return !(!x) & !(x>>31);
 }
 /* 
  * isLessOrEqual - if x <= y  then return 1, else return 0 
@@ -275,7 +312,14 @@ int isPositive(int x) {
  *   Rating: 3
  */
 int isLessOrEqual(int x, int y) {
-  return 2;
+  int neg_x = (~x) + 1;
+  int diff = y + neg_x;
+  //to get the sign of int, shift int to the right by 31 (=111...1 if int<0, = 000...0 if int>=0) and & with 1
+  int sign_diff = (diff>>31) & 1;
+  int sign_x = (x>>31) & 1;
+  int sign_y = (y>>31) & 1;
+  //y>=x either if sign_y=0 and sign_x=1 or if sign_y=sign_x and sign_diff=0
+  return (sign_x & !sign_y) | (!sign_diff & !(sign_x ^ sign_y));
 }
 /*
  * ilog2 - return floor(log base 2 of x), where x > 0
@@ -285,7 +329,27 @@ int isLessOrEqual(int x, int y) {
  *   Rating: 4
  */
 int ilog2(int x) {
-  return 2;
+  //declare variables
+  int res;
+  int is16;
+  int is8;
+  int is4;
+  int is2;
+  int is1;
+  //check if there is 1 in first 16 bits from left, if there is res will be > 16
+  is16 = !(!(x >> 16)) << 4;
+  res = is16;
+  //then check if there is 1 in next 8 bits, if there is res will be > 8 + prev res
+  is8 = !(!(x >> (8+res))) << 3;
+  res += is8;
+  //and so on
+  is4 = !(!(x >> (4+res))) << 2;
+  res += is4;
+  is2 = !(!(x >> (2+res))) << 1;
+  res += is2;
+  is1 = !(!(x >> (1+res)));
+  res += is1;
+  return res;
 }
 /* 
  * float_neg - Return bit-level equivalent of expression -f for
@@ -299,7 +363,10 @@ int ilog2(int x) {
  *   Rating: 2
  */
 unsigned float_neg(unsigned uf) {
- return 2;
+  //check if uf is infinity or NaN
+  if (uf == 0xffc00000 || uf == 0x7fc00000) return uf;
+  //otherwise, set the sign bit of uf to 1
+  return uf ^ (1<<31);
 }
 /* 
  * float_i2f - Return bit-level equivalent of expression (float) x
@@ -311,7 +378,29 @@ unsigned float_neg(unsigned uf) {
  *   Rating: 4
  */
 unsigned float_i2f(int x) {
-  return 2;
+  //we need to set sign bit, fraction and exponent
+  unsigned s = 0;
+  unsigned f = 0; 
+  unsigned e = 127; //bias=127
+  unsigned carry = 0;
+  if (x==0) return x;
+  //if x is negative, we need to change s to 1
+  if (x & 0x80000000) {
+    s = 1;
+    x = -x;
+  }
+  e += 31;
+  while (!(x & 0x80000000)) {
+    x = x<<1;
+    e -= 1;
+  }
+  //we need to transfer last 31 bits of x to f and shift by 8 (since f is 23 bits)
+  f = (x & (~(1<<31))) >> 8;
+  //x might not fit into f, in that case we need to set carry to 1 and add it to f
+  if ((x & 0xff) > 0x80) carry = 1;
+  else if ((x & 0x000001ff) == 0x180) carry = 1;
+  f += carry;
+  return ((s<<31) + (e<<23)) + f;
 }
 /* 
  * float_twice - Return bit-level equivalent of expression 2*f for
@@ -325,5 +414,20 @@ unsigned float_i2f(int x) {
  *   Rating: 4
  */
 unsigned float_twice(unsigned uf) {
-  return 2;
+  //we need to get sign bit(s), exponent(e) and fraction(f) of giving floating point number
+  unsigned s = (uf>>31) & 1;
+  unsigned e = (uf>>23) & 0xff;
+  unsigned f = uf & ~((1<<31)>>8);
+  //check if given argument is NaN or infinity
+  if (e == 0xff) return uf;
+  //if e is maximum, multiplying argument by 2 will overflow (i.e. infinity)
+  if (e == 0xff - 1) {
+    e = 0xff;
+    f = 0;
+  // if argument is denormalized (i.e e=0), just shift f left by 1 (i.e mult by 2)
+  } else if (e==0) f = f << 1;
+  //else - when argument is normalized, just increase exponent by 1
+  else e = e + 1;
+  //combine sign, expoenent and fraction back
+  return ((s<<31) | (e<<23)) | f;
 }
